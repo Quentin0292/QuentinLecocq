a9e5cc7e5b5f9468b159af4cbb2031dc1a81aab9
Quentin <Quentin.lecocq@yahoo.com>
2019-05-31 13:40:37 +0200

    modify case of Layout component import

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
-import pic11 from '../images/logo.png'
 
 const Success = props => (
   <Layout>
